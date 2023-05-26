965631802aa7dac9b3e4e78247ca66e646192d5c
yehui <351610469@qq.com>
2023-05-26 16:07:33 +0800

    feat(deps): 开始eslint配置

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+// import { HashRouter as Router, Routes, Route, Link } from "react-router-dom";
+// import PureComponent from "./routes/PureComponent.jsx";
+// import ReactLazy from "./routes/ReactLazy.jsx";
+
+class App extends Component {
+  render() {
+    return (
+      <>1111 <>1111</></>
+      // <Router>
+      //   <ul>
+      //     <li>
+      //       <Link to="/PureComponent">PureComponent</Link>
+      //     </li>
+      //     <li>
+      //       <Link to="/ReactLazy">ReactLazy</Link>
+      //     </li>
+      //   </ul>
+      //   <Routes>
+      //     <Route path="/PureComponent" element={<PureComponent />} />
+      //     <Route path="/ReactLazy" element={<ReactLazy />} />
+      //   </Routes>
+      // </Router>
+    );
+  }
+}
+
+export default App;
---
+++ src/main.tsx
@@ -0,0 +1,5 @@
+import { createRoot } from "react-dom/client";
+console.log('1111')
+import App from "./App";
+
+createRoot(document.getElementById("root") as Element).render(<App />);
