6334d1b32954be79957f08fafd7244336248bb5e
yehui <351610469@qq.com>
2023-06-08 23:17:09 +0800

    feat(deps): 完成基本配置，开始加命令功能

---
+++ .prettierrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  pluginSearchDirs: false,
+  plugins: [
+    require.resolve("prettier-plugin-organize-imports"),
+    require.resolve("prettier-plugin-packagejson"),
+  ],
+  proseWrap: "never",
+  useTabs: false,
+  tabWidth: 2,
+  printWidth: 100,
+  singleQuote: false,
+  trailingComma: "all",
+  bracketSpacing: true,
+  semi: true,
+  "prettier.semi": true,
+  overrides: [
+    {
+      files: "*.md",
+      options: {
+        proseWrap: "preserve",
+      },
+    },
+  ],
+};
